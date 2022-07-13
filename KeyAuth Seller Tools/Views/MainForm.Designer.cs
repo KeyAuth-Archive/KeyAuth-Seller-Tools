586a5171326256702ce639cd476928184c77a5df
lIMonkieIl <a_brine@yahoo.com>
2022-07-13 13:16:06 +0100

    1

--- KeyAuth Seller Tools/Views/MainForm.Designer.cs
+++ KeyAuth Seller Tools/Views/MainForm.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HasherBTN = new System.Windows.Forms.Button();
+            this.LicenseViewBTN = new System.Windows.Forms.Button();
+            this.ViewsPNL = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.HasherBTN);
+            this.panel1.Controls.Add(this.LicenseViewBTN);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 28);
+            this.panel1.TabIndex = 0;
+            // 
+            // HasherBTN
+            // 
+            this.HasherBTN.Dock = System.Windows.Forms.DockStyle.Left;
+            this.HasherBTN.Location = new System.Drawing.Point(107, 0);
+            this.HasherBTN.Name = "HasherBTN";
+            this.HasherBTN.Size = new System.Drawing.Size(107, 28);
+            this.HasherBTN.TabIndex = 1;
+            this.HasherBTN.Text = "Hasher";
+            this.HasherBTN.UseVisualStyleBackColor = true;
+            this.HasherBTN.Click += new System.EventHandler(this.HasherBTN_Click);
+            // 
+            // LicenseViewBTN
+            // 
+            this.LicenseViewBTN.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LicenseViewBTN.Location = new System.Drawing.Point(0, 0);
+            this.LicenseViewBTN.Name = "LicenseViewBTN";
+            this.LicenseViewBTN.Size = new System.Drawing.Size(107, 28);
+            this.LicenseViewBTN.TabIndex = 0;
+            this.LicenseViewBTN.Text = "License Remover";
+            this.LicenseViewBTN.UseVisualStyleBackColor = true;
+            this.LicenseViewBTN.Click += new System.EventHandler(this.LicenseViewBTN_Click);
+            // 
+            // ViewsPNL
+            // 
+            this.ViewsPNL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewsPNL.Location = new System.Drawing.Point(0, 28);
+            this.ViewsPNL.Name = "ViewsPNL";
+            this.ViewsPNL.Size = new System.Drawing.Size(316, 422);
+            this.ViewsPNL.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(316, 450);
+            this.Controls.Add(this.ViewsPNL);
+            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KeyAuth Seller Tools";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button HasherBTN;
+        private Button LicenseViewBTN;
+        private Panel ViewsPNL;
     }
 }
--- KeyAuth Seller Tools/Views/MainForm.cs
+++ KeyAuth Seller Tools/Views/MainForm.cs
@@ -1,3 +1,5 @@
+using KeyAuth_Seller_Tools.Views;
+
 namespace KeyAuth_Seller_Tools
 {
     public partial class MainForm : Form
@@ -5,6 +7,27 @@ namespace KeyAuth_Seller_Tools
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void LicenseViewBTN_Click(object sender, EventArgs e)
+        {
+            ViewsPNL.Controls.Clear();
+            LicenseView licenseView = new LicenseView();
+            ViewsPNL.Controls.Add(licenseView);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ViewsPNL.Controls.Clear();
+            LicenseView licenseView = new LicenseView();
+            ViewsPNL.Controls.Add(licenseView);
+        }
+
+        private void HasherBTN_Click(object sender, EventArgs e)
+        {
+            ViewsPNL.Controls.Clear();
+            HasherView hasherView = new HasherView();
+            ViewsPNL.Controls.Add(hasherView);
         }
     }
 }
